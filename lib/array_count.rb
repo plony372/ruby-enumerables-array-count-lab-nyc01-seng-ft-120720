d59fd9d7aa66de5610fc6f896e8bb5e942e22471
plony benav <menachemfuterfas@gmail.com>
2020-11-14 23:47:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,6 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  new = array.count
-  new
+  
 end
 
 def count_empty_strings(array)
